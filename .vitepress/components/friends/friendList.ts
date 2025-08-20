878d8febf83a60943dd005d0eb44c3da534f8b66
March7th <march7th@0721.uk>
2025-08-20 23:52:40 +0800

    test add friends list

---
+++ .vitepress/components/friends/friendList.ts
@@ -0,0 +1,18 @@
+interface FriendList {
+  avatar: string
+  name: string
+  link: string
+}
+
+export const friendList: FriendList[] = [
+  {
+    avatar: '/avatar/AnthonySummit.webp',
+    name: 'Anthony Summit',
+    link: 'https://csmoe.top/',
+  },
+  {
+    avatar: '/avatar/listder.webp',
+    name: 'listder',
+    link: 'https://blog.listder.xyz/',
+  },
+]
